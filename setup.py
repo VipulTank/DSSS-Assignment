d19fc789a72eff21dbd41b21331a05b5387a620b
VipulTank <tankvipul18@gmail.com>
2023-11-13 23:10:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup( name="DSSS_Homework2",
+setup(name="DSSS_Homework2",
       version="0.1", 
       author="Vipul", 
       author_email="tankvipul18@gmail.com",
       packages=find_packages(),
-      install_requires=["numpy"] )
+      install_requires=["numpy"])
